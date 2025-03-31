20635a56795684aa8b5b0953d0c55c0d8282070b
53175DDD <143765653+53175ddd@users.noreply.github.com>
2025-03-31 15:10:30 +0900

    保存できてなかった分の更新

--- src/sound.cpp
+++ src/sound.cpp
@@ -5,28 +5,32 @@ void culc_freq_table(const float pitch) {
   
   f = pitch;
 
-  freq[48] = (uint16_t)round(f);
+  freq_table[48] = (uint16_t)round(f);
 
   for(int8_t i = 47; 0 <= i; i--) {
     f = f / pow(2, 1/12);
-    freq[i] = (uint16_t)round(f);
+    freq_table[i] = (uint16_t)round(f);
   }
 
   f = pitch;
 
   for(int8_t i = 49; i < 88; i++) {
     f = f * pow(2, 1/12);
-    freq[i] = (uint16_t)round(f);
+    freq_table[i] = (uint16_t)round(f);
   }
 }
 
-void play(uint8_t spk, score_t score[], uint16_t count, float tempo) {
+void set_speaker_out(uint8_t spk) {
+  _spk = spk;
+}
+
+void play(score_t score[], uint16_t count, float tempo) {
   for(uint16_t i = 0; i < count; i++) {
-    uint16_t f = freq_table[score[i].note_freq];  
+    uint16_t f = freq_table[score[i].note_number];  
     uint16_t p = score[i].tone_period * (60 / tempo) * 1000;
     uint16_t m = score[i].mute_period * (60 / tempo) * 1000;
 
-    tone(spk, f, p);
+    tone(_spk, f, p);
     delay(p + m);
   }
 }
