865559d06953aae50e2560abce09e5873c205ed6
53175DDD <143765653+53175ddd@users.noreply.github.com>
2025-04-02 16:30:34 +0900

    サンプルスコア修正

--- example/simple_sample/simple_sample.ino
+++ example/simple_sample/simple_sample.ino
@@ -4,7 +4,10 @@
 
 #define SPK 14  // adapt to your circuit
 
+sound music(CONCERT_PITCH);
+
 // definition of score_t
+//
 // typedef struct {
 //   uint8_t  note_number;  // note number. A4 is defined as 48.
 //   uint16_t tone_period;  // length of it.
@@ -14,17 +17,20 @@
 score_t ode_to_joy[] = {{C5, QUARTER_NOTE - 10, 10}, {C5, QUARTER_NOTE, 0}, {Cs5, QUARTER_NOTE, 0}, {Ds5, QUARTER_NOTE - 10, 10}, {Ds5, QUARTER_NOTE, 0}, {Cs5, QUARTER_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {As4, QUARTER_NOTE, 0},
                         {Gs4, QUARTER_NOTE - 10, 10}, {Gs4, QUARTER_NOTE, 0}, {As4, QUARTER_NOTE, 0}, {C5, QUARTER_NOTE - 10, 10}, {C5, DOTTED_QUARTER_NOTE, 0}, {As4, EIGHTH_NOTE - 10, 10}, {As4, HALF_NOTE, 0},
                         {C5, QUARTER_NOTE - 10, 10}, {C5, QUARTER_NOTE, 0}, {Cs5, QUARTER_NOTE, 0}, {Ds5, QUARTER_NOTE - 10, 10}, {Ds5, QUARTER_NOTE, 0}, {Cs5, QUARTER_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {As4, QUARTER_NOTE, 0},
-                        {Gs4, QUARTER_NOTE - 10, 10}, {As4, QUARTER_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {As4, DOTTED_HALF_NOTE, 0}, {Fs4, EIGHTH_NOTE - 10, 10}, {Fs4, HALF_NOTE, 0},
-                        {As4, QUARTER_NOTE - 10, 10}, {As4, QUARTER_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {Gs4, QUARTER_NOTE, 0}, {As4, HALF_NOTE, 0}, {C5, EIGHTH_NOTE, 0}, {Cs5, EIGHTH_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {Gs4, QUARTER_NOTE, 0},
-                        {As4, HALF_NOTE, 0}, {C5, EIGHTH_NOTE, 0}, {Cs5, EIGHTH_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {Gs4, QUARTER_NOTE, 0},{Gs4, QUARTER_NOTE, 0}, {As4, QUARTER_NOTE, 0}, {Bs4, QUARTER_NOTE, 0},
+                        {Gs4, QUARTER_NOTE - 10, 10}, {Gs4, QUARTER_NOTE, 0}, {As4, QUARTER_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {As4, DOTTED_QUARTER_NOTE, 0}, {Gs4, EIGHTH_NOTE - 10, 10}, {Gs4, HALF_NOTE, 0},
+                        {As4, QUARTER_NOTE - 10, 10}, {As4, QUARTER_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {Gs4, QUARTER_NOTE, 0}, {As4, QUARTER_NOTE, 0}, {C5, EIGHTH_NOTE, 0}, {Cs5, EIGHTH_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {Gs4, QUARTER_NOTE, 0},
+                        {As4, QUARTER_NOTE, 0}, {C5, EIGHTH_NOTE, 0}, {Cs5, EIGHTH_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {Gs4, QUARTER_NOTE, 0}, {Gs4, QUARTER_NOTE, 0}, {As4, QUARTER_NOTE, 0}, {Ds4, QUARTER_NOTE, 0},
                         {C5, HALF_NOTE - 10, 10}, {C5, QUARTER_NOTE, 0}, {Cs5, QUARTER_NOTE, 0}, {Ds5, QUARTER_NOTE - 10, 10}, {Ds5, QUARTER_NOTE, 0}, {Cs5, QUARTER_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {Cs5, EIGHTH_NOTE, 0}, {As4, EIGHTH_NOTE, 0},
                         {Gs4, QUARTER_NOTE - 10, 10}, {Gs4, QUARTER_NOTE, 0}, {As4, QUARTER_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {As4, DOTTED_QUARTER_NOTE, 0}, {Gs4, EIGHTH_NOTE - 10, 10}, {Gs4, DOTTED_HALF_NOTE, DOTTED_HALF_NOTE}};
 
 void setup() {
-  culc_freq_table(CONCERT_PITCH);
-  set_speaker_out(SPK);
+  Serial.begin(115200);
 
-  play(ode_to_joy, sizeof(ode_to_joy) / sizeof(score_t), 124);
+  music.set_speaker_out(SPK);
+  music.generate();
+  music.debug_table();
+
+  music.play(ode_to_joy, sizeof(ode_to_joy) / sizeof(score_t), 124);
 }
 
 void loop() {
