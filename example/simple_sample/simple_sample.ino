29825f7447edb8acee42f58b76adc5b7b6e0d122
53175DDD <143765653+53175ddd@users.noreply.github.com>
2025-04-03 00:24:18 +0900

    移調に対応

--- example/simple_sample/simple_sample.ino
+++ example/simple_sample/simple_sample.ino
@@ -2,26 +2,27 @@
 
 #define CONCERT_PITCH 440.0f
 
-#define SPK 14  // adapt to your circuit
+#define SPK   14  // adapt to your circuit
+#define SHIFT  3
 
 sound music(CONCERT_PITCH);
 
 // definition of score_t
 //
 // typedef struct {
-//   uint8_t  note_number;  // note number. A4 is defined as 48.
+//   uint8_t  note_number;  // note number. A4 + SHIFT is defined as 4 + SHIFT8.
 //   uint16_t tone_period;  // length of it.
 //   uint16_t mute_period;  // length of muted time after each note. normaly zero.
 // } score_t;
 
-score_t ode_to_joy[] = {{C5, QUARTER_NOTE - 10, 10}, {C5, QUARTER_NOTE, 0}, {Cs5, QUARTER_NOTE, 0}, {Ds5, QUARTER_NOTE - 10, 10}, {Ds5, QUARTER_NOTE, 0}, {Cs5, QUARTER_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {As4, QUARTER_NOTE, 0},
-                        {Gs4, QUARTER_NOTE - 10, 10}, {Gs4, QUARTER_NOTE, 0}, {As4, QUARTER_NOTE, 0}, {C5, QUARTER_NOTE - 10, 10}, {C5, DOTTED_QUARTER_NOTE, 0}, {As4, EIGHTH_NOTE - 10, 10}, {As4, HALF_NOTE, 0},
-                        {C5, QUARTER_NOTE - 10, 10}, {C5, QUARTER_NOTE, 0}, {Cs5, QUARTER_NOTE, 0}, {Ds5, QUARTER_NOTE - 10, 10}, {Ds5, QUARTER_NOTE, 0}, {Cs5, QUARTER_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {As4, QUARTER_NOTE, 0},
-                        {Gs4, QUARTER_NOTE - 10, 10}, {Gs4, QUARTER_NOTE, 0}, {As4, QUARTER_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {As4, DOTTED_QUARTER_NOTE, 0}, {Gs4, EIGHTH_NOTE - 10, 10}, {Gs4, HALF_NOTE, 0},
-                        {As4, QUARTER_NOTE - 10, 10}, {As4, QUARTER_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {Gs4, QUARTER_NOTE, 0}, {As4, QUARTER_NOTE, 0}, {C5, EIGHTH_NOTE, 0}, {Cs5, EIGHTH_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {Gs4, QUARTER_NOTE, 0},
-                        {As4, QUARTER_NOTE, 0}, {C5, EIGHTH_NOTE, 0}, {Cs5, EIGHTH_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {Gs4, QUARTER_NOTE, 0}, {Gs4, QUARTER_NOTE, 0}, {As4, QUARTER_NOTE, 0}, {Ds4, QUARTER_NOTE, 0},
-                        {C5, HALF_NOTE - 10, 10}, {C5, QUARTER_NOTE, 0}, {Cs5, QUARTER_NOTE, 0}, {Ds5, QUARTER_NOTE - 10, 10}, {Ds5, QUARTER_NOTE, 0}, {Cs5, QUARTER_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {Cs5, EIGHTH_NOTE, 0}, {As4, EIGHTH_NOTE, 0},
-                        {Gs4, QUARTER_NOTE - 10, 10}, {Gs4, QUARTER_NOTE, 0}, {As4, QUARTER_NOTE, 0}, {C5, QUARTER_NOTE, 0}, {As4, DOTTED_QUARTER_NOTE, 0}, {Gs4, EIGHTH_NOTE - 10, 10}, {Gs4, DOTTED_HALF_NOTE, DOTTED_HALF_NOTE}};
+score_t ode_to_joy[] = {{C5 + SHIFT, QUARTER_NOTE - 10, 10}, {C5 + SHIFT, QUARTER_NOTE, 0}, {Cs5 + SHIFT, QUARTER_NOTE, 0}, {Ds5 + SHIFT, QUARTER_NOTE - 10, 10}, {Ds5 + SHIFT, QUARTER_NOTE, 0}, {Cs5 + SHIFT, QUARTER_NOTE, 0}, {C5 + SHIFT, QUARTER_NOTE, 0}, {As4 + SHIFT, QUARTER_NOTE, 0},
+                        {Gs4 + SHIFT, QUARTER_NOTE - 10, 10}, {Gs4 + SHIFT, QUARTER_NOTE, 0}, {As4 + SHIFT, QUARTER_NOTE, 0}, {C5 + SHIFT, QUARTER_NOTE - 10, 10}, {C5 + SHIFT, DOTTED_QUARTER_NOTE, 0}, {As4 + SHIFT, EIGHTH_NOTE - 10, 10}, {As4 + SHIFT, HALF_NOTE, 0},
+                        {C5 + SHIFT, QUARTER_NOTE - 10, 10}, {C5 + SHIFT, QUARTER_NOTE, 0}, {Cs5 + SHIFT, QUARTER_NOTE, 0}, {Ds5 + SHIFT, QUARTER_NOTE - 10, 10}, {Ds5 + SHIFT, QUARTER_NOTE, 0}, {Cs5 + SHIFT, QUARTER_NOTE, 0}, {C5 + SHIFT, QUARTER_NOTE, 0}, {As4 + SHIFT, QUARTER_NOTE, 0},
+                        {Gs4 + SHIFT, QUARTER_NOTE - 10, 10}, {Gs4 + SHIFT, QUARTER_NOTE, 0}, {As4 + SHIFT, QUARTER_NOTE, 0}, {C5 + SHIFT, QUARTER_NOTE, 0}, {As4 + SHIFT, DOTTED_QUARTER_NOTE, 0}, {Gs4 + SHIFT, EIGHTH_NOTE - 10, 10}, {Gs4 + SHIFT, HALF_NOTE, 0},
+                        {As4 + SHIFT, QUARTER_NOTE - 10, 10}, {As4 + SHIFT, QUARTER_NOTE, 0}, {C5 + SHIFT, QUARTER_NOTE, 0}, {Gs4 + SHIFT, QUARTER_NOTE, 0}, {As4 + SHIFT, QUARTER_NOTE, 0}, {C5 + SHIFT, EIGHTH_NOTE, 0}, {Cs5 + SHIFT, EIGHTH_NOTE, 0}, {C5 + SHIFT, QUARTER_NOTE, 0}, {Gs4 + SHIFT, QUARTER_NOTE, 0},
+                        {As4 + SHIFT, QUARTER_NOTE, 0}, {C5 + SHIFT, EIGHTH_NOTE, 0}, {Cs5 + SHIFT, EIGHTH_NOTE, 0}, {C5 + SHIFT, QUARTER_NOTE, 0}, {Gs4 + SHIFT, QUARTER_NOTE, 0}, {Gs4 + SHIFT, QUARTER_NOTE, 0}, {As4 + SHIFT, QUARTER_NOTE, 0}, {Ds4 + SHIFT, QUARTER_NOTE, 0},
+                        {C5 + SHIFT, HALF_NOTE - 10, 10}, {C5 + SHIFT, QUARTER_NOTE, 0}, {Cs5 + SHIFT, QUARTER_NOTE, 0}, {Ds5 + SHIFT, QUARTER_NOTE - 10, 10}, {Ds5 + SHIFT, QUARTER_NOTE, 0}, {Cs5 + SHIFT, QUARTER_NOTE, 0}, {C5 + SHIFT, QUARTER_NOTE, 0}, {Cs5 + SHIFT, EIGHTH_NOTE, 0}, {As4 + SHIFT, EIGHTH_NOTE, 0},
+                        {Gs4 + SHIFT, QUARTER_NOTE - 10, 10}, {Gs4 + SHIFT, QUARTER_NOTE, 0}, {As4 + SHIFT, QUARTER_NOTE, 0}, {C5 + SHIFT, QUARTER_NOTE, 0}, {As4 + SHIFT, DOTTED_QUARTER_NOTE, 0}, {Gs4 + SHIFT, EIGHTH_NOTE - 10, 10}, {Gs4 + SHIFT, DOTTED_HALF_NOTE, DOTTED_HALF_NOTE}};
 
 void setup() {
   Serial.begin(115200);
